4990803844d33ed4b446159229df90811b061060
irakozejules <mirakozejules@gmail.com>
2023-05-22 12:13:06 +0200

    [delivers #185218908] chore(setup): sass setup
    - Setup sass processing in project
    - Modification of default package.json generated for a better start up

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './styles/index.scss'
 import { Provider } from 'react-redux';
 import store from './states/store.js';
 
