f57a173f3b06102792a4e3cb80af43b5f7b97712
Nishimwe Prince <princeelysee@gmail.com>
2023-06-05 13:00:30 +0200

    bug-fix (code refactor) fixing database links after redeployment
    - adds the backend API URL in environment variables for security
    - updates the styles to remove duplication
    - updates components to make them more customizable and remove repetition

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -7,14 +7,16 @@ const Hero = () => {
     <div className='home-hero flex flex-col screen-mid:flex-col'>
       <span className='home-hero-container grid grid-cols-2 screen-mid:grid-cols-1'>
         <section className='home-hero-text'>
-          <p>New Arrivals</p>
+          <p style={{
+            textTransform: 'uppercase',
+          }}>New Arrivals</p>
           <div>
             <h1>Feed Your Passion</h1>
             <p>And get a bargain in the process</p>
           </div>
           <p>$24</p>
           <Button
-            route='/shop'
+            route='/login'
             className='hero-btn primary-btn'
             value='Buy now'
           />
